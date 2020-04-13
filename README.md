c546229829f9d63d2e19323c1c96734a36b191c9
Daniel Moran <danmoran@broadinstitute.org>
2020-04-13 12:01:51 -0400

    DSPDC-973 First version of action (#1)

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# setup-chart-releaser
-Set up your GitHub Actions workflow with a specific version of chart-releaser
+# Setup Chart Releaser
+**Set up your GitHub Actions workflow with a specific version of chart-releaser**
+
+Acceptable values are `'latest'` or the semantic version of a tagged release.
+Use this action in a workflow to define which version of `cr` will be used:
+```yaml
+- uses: broadinstitute/setup-chart-releaser@master
+  with:
+    version: latest # Or a pinned version
+```
+The cached `cr` binary path (`cr.exe` on Windows hosts) is prepended to the `PATH`
+environment variable as well as stored in the `chart-releaser-path` output variable.
+
+## Acknowledgements
+The logic in this action is based heavily off of Azure's
+[`setup-helm`](https://github.com/Azure/setup-helm) action.
---
+++ index.js
@@ -0,0 +1,130 @@
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const util = require('util');
+
+const core = require('@actions/core');
+const toolCache = require('@actions/tool-cache');
+
+/** Get the expected file extension for the cr binary. */
+function getExecutableExtension() {
+  if (os.type().match(/^Win/)) {
+    return '.exe';
+  } else {
+    return '';
+  }
+}
+
+/** Get the download URL of the cr binary for the host running the action. */
+function getDownloadUrl(version) {
+  const downloadPattern = 'https://github.com/helm/chart-releaser/releases/download/v%s/chart-releaser_%s_%s';
+
+  switch (os.type()) {
+    case 'Linux':
+      return util.format(downloadPattern, version, version, 'linux_amd64.tar.gz');
+    case 'Darwin':
+      return util.format(downloadPattern, version, version, 'darwin_amd64.tar.gz');
+    default:
+      return util.format(downloadPattern, version, version, 'windows_amd64.zip');
+  }
+}
+
+/** Get the tag of the latest available chart-releaser version. */
+async function getLatestVersion() {
+  const latestReleaseUrl = 'https://api.github.com/repos/helm/chart-releaser/releases/latest';
+
+  return toolCache.downloadTool(latestReleaseUrl).then((downloadPath) => {
+    const response = JSON.parse(fs.readFileSync(downloadPath, 'utf8').toString().trim());
+    const rawTag = response.tag_name
+    if (rawTag.charAt(0) === 'v') {
+      return rawTag.substr(1);
+    } else {
+      return rawTag;
+    }
+  }, (error) => {
+    core.debug(error);
+    throw new Error(`Failed to get latest version of chart-releaser from ${latestReleaseUrl}`);
+  });
+}
+
+/** Walk a directory, collecting paths to all files with a given name. */
+function walkSync(dir, fileList, fileToFind) {
+  const files = fs.readdirSync(dir);
+  fileList = fileList || [];
+  files.forEach((file) => {
+    if (fs.statSync(path.join(dir, file)).isDirectory()) {
+      fileList = walkSync(path.join(dir, file), fileList, fileToFind);
+    } else {
+      core.debug(file);
+      if (file == fileToFind) {
+        fileList.push(path.join(dir, file));
+      }
+    }
+  });
+  return fileList;
+}
+
+/** Find the chart-releaser executable in a directory tree. */
+function findExecutable(rootFolder) {
+  fs.chmodSync(rootFolder, '777');
+  var fileList = [];
+  const executableName = 'cr' + getExecutableExtension();
+  walkSync(rootFolder, fileList, executableName);
+  if (!fileList) {
+    throw new Error(`${executableName} executable not found in path ${rootFolder}`);
+  } else {
+    return fileList[0];
+  }
+}
+
+/** Download a specific version of chart-releaser. */
+async function downloadChartReleaser(version) {
+  const toolName = 'chart-releaser';
+
+  let cachedToolPath = toolCache.find(toolName, version);
+  if (!cachedToolPath) {
+    const downloadUrl = getDownloadUrl(version);
+    let downloadPath;
+    try {
+      downloadPath = await toolCache.downloadTool(downloadUrl);
+    } catch (exception) {
+      core.debug(exception);
+      throw new Error(`Failed to download chart-releaser from ${downloadUrl}`);
+    }
+
+    fs.chmodSync(downloadPath, '777');
+    let unzippedPath;
+    switch (os.type()) {
+      case 'Linux':
+      case 'Darwin':
+        unzippedPath = await toolCache.extractTar(downloadPath);
+        break;
+      default:
+        unzippedPath = await toolCache.extractZip(downloadPath);
+    }
+    cachedToolPath = await toolCache.cacheDir(unzippedPath, toolName, version);
+  }
+
+  const executablePath = findExecutable(cachedToolPath);
+  fs.chmodSync(executablePath, '777');
+  return executablePath;
+}
+
+async function run() {
+  let version = core.getInput('version', { 'required': true });
+  if (version.toLocaleLowerCase() === 'latest') {
+    version = await getLatestVersion();
+  }
+
+  let executablePath = await downloadChartReleaser(version);
+  let executableDir = path.dirname(executablePath);
+
+  if (!process.env['PATH'].startsWith(executableDir)) {
+    core.addPath(executableDir);
+  }
+
+  console.log(`Helm tool version ${version} has been cached at ${executablePath}`);
+  core.setOutput('chart-releaser-path', executablePath);
+}
+
+run().catch(core.setFailed);
